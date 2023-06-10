566896cdb2ace4e38ab6066be6a65ba44fcdf3a0
dmastrop <dave.mastropolo@gmail.com>
2023-06-10 13:03:56 -0700

    added direct cluster addrss to database.js

--- data/database.js
+++ data/database.js
@@ -7,7 +7,7 @@ const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 //const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const uri = `${connectionProtocol}://dave:8JJDseGJYGqTbtsc@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://dave:8JJDseGJYGqTbtsc@cluster0.pj1lxuo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
